f0486705ed0b9dad244993c9ccb59cfbf7f58c9c
RoberteFlaherty <45772389+RoberteFlaherty@users.noreply.github.com>
2020-08-10 16:53:30 -0800

    trying to get the email

--- Script-5.sql
+++ Script-5.sql
@@ -1,4 +1,4 @@
 select public.app_user.first_name ,public.app_user.last_name ,public.app_user."password",public.app_user.role_id, 
-public.app_user.user_id,public.app_user.username 
+public.app_user.user_id,public.app_user.username,public.study_set."name",public.study_set.owner_id,public.study_set.study_set_id 
 FROM public.app_user 
-INNER JOIN public.study_set ON public.app_user.user_id =public.study_set.owner_id where public.study_set.owner_id =4;
+full JOIN public.study_set ON public.app_user.user_id =public.study_set.owner_id where public.study_set.owner_id =4;
