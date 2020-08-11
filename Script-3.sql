eaf2dc166618f86d7531553f547d0423ec339a11
RoberteFlaherty <45772389+RoberteFlaherty@users.noreply.github.com>
2020-08-10 17:14:24 -0800

    commit

--- Script-3.sql
+++ Script-3.sql
@@ -1,3 +1,1 @@
-INSERT INTO public.app_user
-(user_id ,username, "password", first_name, last_name, role_id)
-VALUES(22,'javaguy', 'robert', 'robert', 'flaherty', 4);
+INSERT INTO public.app_user(user_id ,username, "password", first_name, last_name, role_id)VALUES(22,'javaguy', 'robert', 'robert', 'flaherty', 4);
--- Script-5.sql
+++ Script-5.sql
@@ -1,4 +1,1 @@
-select public.app_user.first_name ,public.app_user.last_name ,public.app_user."password",public.app_user.role_id, 
-public.app_user.user_id,public.app_user.username,public.study_set."name",public.study_set.owner_id,public.study_set.study_set_id 
-FROM public.app_user 
-full JOIN public.study_set ON public.app_user.user_id =public.study_set.owner_id where public.app_user.user_id=5;
+select public.app_user.first_name ,public.app_user.last_name ,public.app_user."password",public.app_user.role_id, public.app_user.user_id,public.app_user.username,public.study_set."name",public.study_set.owner_id,public.study_set.study_set_id FROM public.app_user full JOIN public.study_set ON public.app_user.user_id =public.study_set.owner_id where public.app_user.user_id=5;
